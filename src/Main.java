1b4481600ae8bfc27c769c016118d81729c2354c
chawki <chouibc@gmail.com>
2016-10-30 09:38:41 +0100

    creation du main

--- src/Main.java
+++ src/Main.java
@@ -10,12 +10,32 @@ public class Main {
         Point B = new Point(5, 5);
         Point C = new Point(3, 8);
 
+
+
         Segment AB = new Segment(A, B);
         Segment BC = new Segment(B, C);
 
         Cercle cercle1 = new Cercle(A, AB);
         Cercle cercle2 = new Cercle(B, BC);
 
+
+        PointNom point1 = new PointNom("X", 10, 5);
+        PointNom point2 = new PointNom("Y", 2);
+
+        System.out.println("---UPDATE----");
+
+        System.out.println("Avant:");
+        point1.afficheCor();//afficher le point
+        point2.afficheCor();
+
+        point1.deplace(5, -5);//deplacer le point
+        point2.deplace(1,2);
+
+        System.out.println("\nApres:");
+        point1.afficheCor();//afficher le nouveau deplacement
+        point2.afficheCor();
+
+        System.out.println("---UPDATE----");
 
         System.out.println("A" + A.toString() + "\nB" + B.toString() + "\nC" + C.toString());
         System.out.println("\nimage de A = A-1" + A.symetrie().toString() + "\nimage de B = B-1" + B.symetrie().toString() + "\nimage de C = C-1" + C.symetrie().toString());
