3cf61d79b5399e1ed138f76c9739b785154e30da
chawki <chouibc@gmail.com>
2016-10-30 09:41:01 +0100

    creation du main

--- src/PointA.java
+++ src/PointA.java
@@ -1,18 +1,18 @@
 /**
  * Created by HP on 30/10/2016.
  */
-    public class PointA extends Point {
+public class PointA extends Point {
 
-             public PointA( int x, int y) {
-                    super(x, y);
-                }
+    public PointA(int x, int y) {
+        super(x, y);
+    }
 
-            public PointA(String nom, int x) {
-                   super( x);
-               }
+    public PointA(int x) {
+        super(x);
+    }
 
 
-             public void afficheCor() {
-                    System.out.println( "( " + getAbscisse() + " , " + getOrdonnee() + " )");
-                }
- }
+    public void afficheCor() {
+        System.out.println("( " + getAbscisse() + " , " + getOrdonnee() + " )");
+    }
+}
